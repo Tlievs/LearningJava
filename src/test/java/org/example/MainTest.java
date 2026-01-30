baae6fd76a7a0c5375d340cd8be6478dab66252b
maksim.gorchakov <laptop@learning.com>
2026-01-31 00:21:55 +0300

    Completed isEverywhere, matchUp, has77, has12, modThree, haveThree and wrote basic test for them

--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -29,4 +29,45 @@ class MainTest {
         assertTrue(main.isEverywhere(new int[]{1, 2, 1, 3}, 1));
         assertFalse(main.isEverywhere(new int[]{1, 2, 4, 3}, 1));
     }
+
+    @Test
+    void testMatchUp() {
+        Main main = new Main();
+        assertEquals(main.matchUp(new int[]{1, 2, 3}, new int[]{2, 3, 10}), 2);
+        assertEquals(main.matchUp(new int[]{1, 2, 3}, new int[]{2, 3, 5}), 3);
+        assertEquals(main.matchUp(new int[]{1, 2, 3}, new int[]{2, 3, 3}), 2);
+    }
+
+    @Test
+    void testHas77() {
+        Main main = new Main();
+        assertTrue(main.has77(new int[]{1, 7, 7}));
+        assertTrue(main.has77(new int[]{1, 7, 1, 7}));
+        assertFalse(main.has77(new int[]{1, 7, 1, 1, 7}));
+        assertTrue(main.has77(new int[]{7, 7, 1, 1, 7}));
+    }
+
+    @Test
+    void testHas12() {
+        Main main = new Main();
+        assertTrue(main.has12(new int[]{1, 3, 2}));
+        assertTrue(main.has12(new int[]{3, 1, 2}));
+        assertTrue(main.has12(new int[]{3, 1, 4, 5, 2}));
+    }
+
+    @Test
+    void testModThree() {
+        Main main = new Main();
+        assertTrue(main.modThree(new int[]{2, 1, 3, 5}));
+        assertFalse(main.modThree(new int[]{2, 1, 2, 5}));
+        assertTrue(main.modThree(new int[]{2, 4, 2, 5}));
+    }
+
+    @Test
+    void testHaveThree() {
+        Main main = new Main();
+        assertTrue(main.haveThree(new int[]{3, 1, 3, 1, 3}));
+        assertFalse(main.haveThree(new int[]{3, 1, 3, 3}));
+        assertFalse(main.haveThree(new int[]{3, 4, 3, 3, 4}));
+    }
 }
