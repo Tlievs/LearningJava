326641924c394c40b298e4a4b9e03bf1e5374759
Tlievs <antiwinlocer.spasibo@gmail.com>
2026-02-15 20:47:24 +0300

    Revert "Clear"

---
+++ src/main/java/OOP/Main.java
@@ -0,0 +1,23 @@
+package OOP;
+
+import java.util.HashMap;
+
+public class Main {
+    public static void main(String[] args) {
+//        ArrayList<Vehicle> vehicles = new ArrayList<>();
+//        vehicles.add(new Car("Tiguan", 100, false));
+//        vehicles.add(new Car("Model 3", 150, true));
+//        vehicles.add(new Truck("TCX", 150, 10));
+//        for (Vehicle vehicle : vehicles) {
+//            vehicle.startEngine();
+//            vehicle.charge();
+//        }
+//        System.out.println(vehicles.size());
+//        vehicles.removeFirst();
+//        System.out.println(vehicles.size());
+        HashMap<String, Car> vehicleHashMap = new HashMap<>();
+        vehicleHashMap.put("A020BT799", new Car("Civic", 120, false));
+        vehicleHashMap.put("O000OO78", new Car("Vesta", 100, true));
+        vehicleHashMap.get("O000OO78").displayDetails();
+    }
+}
